dc092a64839a96705ae7e73e26664924e87e7500
RolefH <rolef.heinrich@de.ibm.com>
2018-08-13 14:44:25 +0200

    fallback to default locale when message not found, issue #105

--- com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/i18n/Messages.java
+++ com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/i18n/Messages.java
@@ -1,20 +1,22 @@
 package com.ibm.streamsx.kafka.i18n;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
     private static final String BUNDLE_NAME = "com.ibm.streamsx.kafka.i18n.KafkaMessages"; //$NON-NLS-1$
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle (BUNDLE_NAME);
+    private static final ResourceBundle FALLBACK_RESOURCE_BUNDLE = ResourceBundle.getBundle (BUNDLE_NAME, new Locale ("en", "US"));
 
     private Messages() {
     }
 
     public static String getString(String key) {
         try {
-            return RESOURCE_BUNDLE.getString(key);
+            return getRawMsg (key);
         } catch (MissingResourceException e) {
             return '!' + key + '!';
         }
@@ -22,10 +24,18 @@ public class Messages {
 
     public static String getString(String key, Object... args) {
         try {
-            String msg = RESOURCE_BUNDLE.getString(key);
+            String msg = getRawMsg (key);
             return MessageFormat.format(msg, args);
         } catch (MissingResourceException e) {
             return '!' + key + '!';
+        }
+    }
+
+    private static String getRawMsg (String key) throws MissingResourceException {
+        try {
+            return RESOURCE_BUNDLE.getString(key);
+        } catch (MissingResourceException e) {
+            return FALLBACK_RESOURCE_BUNDLE.getString(key);
         }
     }
 }

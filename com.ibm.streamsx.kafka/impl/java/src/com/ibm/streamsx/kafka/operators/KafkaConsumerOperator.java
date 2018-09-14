2a02b33bef9f86581f493e1318ec6c4d73ea2c70
RolefH <rolef.heinrich@de.ibm.com>
2018-09-14 10:14:49 +0200

    SPL doc KafkaConsumer operator

--- com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/operators/KafkaConsumerOperator.java
+++ com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/operators/KafkaConsumerOperator.java
@@ -53,89 +53,95 @@ import com.ibm.streams.operator.model.PrimitiveOperator;
 public class KafkaConsumerOperator extends AbstractKafkaConsumerOperator {
 
 	public static final String DESC = 
-    		"The KafkaConsumer operator is used to consume messages from Kafka topics. " //$NON-NLS-1$
-    		+ "The operator can be configured to consume messages from one or more topics, " //$NON-NLS-1$
-    		+ "as well as consume messages from specific partitions within topics.\\n" //$NON-NLS-1$
-			+ "\\n" //$NON-NLS-1$
-			+ "# Supported Kafka Version\\n" //$NON-NLS-1$
-			+ "\\n" //$NON-NLS-1$
-			+ "This version of the toolkit supports **Apache Kafka v0.10.2, v0.11.x, and v1.0.x**.\\n" //$NON-NLS-1$
-			+ "\\n" //$NON-NLS-1$
-    		+ "# Kafka Properties\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"The operator implements Kafka's KafkaConsumer API. As a result, it supports all " //$NON-NLS-1$
-    		+ "Kafka properties that are supported by the underlying API. Properties can be " //$NON-NLS-1$
-    		+ "specified in a file or in an application configuration. If specifying properties " //$NON-NLS-1$
-    		+ "via a file, the **propertiesFile** parameter can be used. If specifying properties " //$NON-NLS-1$
-    		+ "in an application configuration, the name of the application configuration must be " //$NON-NLS-1$
-    		+ "specified using the **appConfigName** parameter. \\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"The only property that the user is required to set is the `bootstrap.servers` property, " //$NON-NLS-1$
-    		+ "which points to the Kafka brokers. All other properties are optional. The operator " //$NON-NLS-1$
-    		+ "sets some properties by default to enable users to quickly get started with the operator. " //$NON-NLS-1$
-    		+ "The following lists which properties the operator sets by default: \\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"---\\n" +  //$NON-NLS-1$
-    		"| Property Name | Default Value |\\n" +  //$NON-NLS-1$
-    		"|===|\\n" +  //$NON-NLS-1$
-    		"| client.id | Generated ID in the form: `C-J<JobId>-<operator name>` |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| group.id | Randomly generated ID in the form: `group-<random_string>` |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| key.deserializer | See **Automatic deserialization** section below |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| value.deserializer | See **Automatic deserialization** section below |\\n" +  //$NON-NLS-1$
-            "|---|\\n" +  //$NON-NLS-1$
-            "| auto.commit.enable | `false` |\\n" +  //$NON-NLS-1$
-    		"---\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"**NOTE:** Users can override any of the above properties by explicitly setting the property " //$NON-NLS-1$
-    		+ "value in either a properties file or in an application configuration.\\n" //$NON-NLS-1$
-    		+ "\\n" //$NON-NLS-1$
-    		+ "# Automatic Deserialization\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"The operator will automatically select the appropriate deserializers for the key and message " //$NON-NLS-1$
-    		+ "based on their types. The following table outlines which deserializer will be used given a " //$NON-NLS-1$
-    		+ "particular type: \\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"---\\n" +  //$NON-NLS-1$
-    		"| Deserializer | SPL Types |\\n" +  //$NON-NLS-1$
-    		"|===|\\n" +  //$NON-NLS-1$
-    		"| org.apache.kafka.common.serialization.StringDeserializer | rstring |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| org.apache.kafka.common.serialization.IntegerDeserializer | int32, uint32 |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| org.apache.kafka.common.serialization.LongDeserializer | int64, uint64 |\\n" +  //$NON-NLS-1$
-            "|---|\\n" +  //$NON-NLS-1$
-            "| org.apache.kafka.common.serialization.FloatDeserializer | float32 |\\n" +  //$NON-NLS-1$
-            "|---|\\n" +  //$NON-NLS-1$
-            "| org.apache.kafka.common.serialization.DoubleDeserializer | float64 |\\n" +  //$NON-NLS-1$
-    		"|---|\\n" +  //$NON-NLS-1$
-    		"| org.apache.kafka.common.serialization.ByteArrayDeserializer | blob | \\n" +  //$NON-NLS-1$
-    		"---\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
+    		"The KafkaConsumer operator is used to consume messages from Kafka topics. "
+    		+ "The operator can be configured to consume messages from one or more topics, "
+    		+ "as well as consume messages from specific partitions within topics.\\n"
+			+ "\\n"
+			+ "# Supported Kafka Version\\n"
+			+ "\\n"
+			+ "This version of the toolkit supports **Apache Kafka v0.10.2, v0.11.x, and v1.0.x**.\\n"
+			+ "\\n"
+    		+ "# Kafka Properties\\n" + 
+    		"\\n" + 
+    		"The operator implements Kafka's KafkaConsumer API. As a result, it supports all "
+    		+ "Kafka properties that are supported by the underlying API. The consumer properties "
+    		+ "can be found in the [https://kafka.apache.org/documentation/#consumerconfigs|Apache Kafka documentation]. "
+    		+ "Properties can be "
+    		+ "specified in a file or in an application configuration. If specifying properties "
+    		+ "via a file, the **propertiesFile** parameter can be used. If specifying properties "
+    		+ "in an application configuration, the name of the application configuration must be "
+    		+ "specified using the **appConfigName** parameter. \\n" + 
+    		"\\n" + 
+    		"The only property that the user is required to set is the `bootstrap.servers` property, "
+    		+ "which points to the Kafka brokers, for example `kafka-0.mydomain:9092,kafka-1.mydomain:9092,kafka-2.mydomain:9092`. "
+    		+ "All other properties are optional. The operator "
+    		+ "sets or adjusts some properties by default to enable users to quickly get started with the operator. "
+    		+ "The following lists which properties the operator sets by default: \\n" + 
+    		"\\n" + 
+    		"---\\n" + 
+    		"| Property Name | Default Value |\\n" + 
+    		"|===|\\n" + 
+    		"| client.id | Generated ID in the form: `C-J<JobId>-<operator name>` |\\n" + 
+    		"|---|\\n" + 
+    		"| group.id | Randomly generated ID in the form: `group-<random_string>` |\\n" + 
+    		"|---|\\n" + 
+    		"| key.deserializer | See **Automatic deserialization** section below |\\n" + 
+    		"|---|\\n" + 
+    		"| value.deserializer | See **Automatic deserialization** section below |\\n" + 
+            "|---|\\n" + 
+            "| auto.commit.enable | adjusted to `false` |\\n" + 
+            "|---|\\n" + 
+            "| max.poll.interval.ms | adjusted to a minimum of *3 \\\\* max (reset timeout, drain timeout)* when in consistent region, 300000 otherwise |\\n" + 
+            "|---|\\n" + 
+            "| metadata.max.age.ms | adjusted to a maximum of 2000 |\\n" + 
+            "|---|\\n" + 
+            "| session.timeout.ms | adjusted to a minimum of 120000 |\\n" + 
+            "|---|\\n" + 
+            "| request.timeout.ms | adjusted to a minimum of 125000 |\\n" + 
+    		"---\\n" + 
+    		"\\n" + 
+    		"**NOTE:** Users can override any of the above properties by explicitly setting the property "
+    		+ "value in either a properties file or in an application configuration.\\n"
+    		+ "\\n"
+    		+ "# Automatic Deserialization\\n" + 
+    		"\\n" + 
+    		"The operator will automatically select the appropriate deserializers for the key and message "
+    		+ "based on their types. The following table outlines which deserializer will be used given a "
+    		+ "particular type: \\n" + 
+    		"\\n" + 
+    		"---\\n" + 
+    		"| Deserializer | SPL Types |\\n" + 
+    		"|===|\\n" + 
+    		"| org.apache.kafka.common.serialization.StringDeserializer | rstring |\\n" + 
+    		"|---|\\n" + 
+    		"| org.apache.kafka.common.serialization.IntegerDeserializer | int32, uint32 |\\n" + 
+    		"|---|\\n" + 
+    		"| org.apache.kafka.common.serialization.LongDeserializer | int64, uint64 |\\n" + 
+            "|---|\\n" + 
+            "| org.apache.kafka.common.serialization.FloatDeserializer | float32 |\\n" + 
+            "|---|\\n" + 
+            "| org.apache.kafka.common.serialization.DoubleDeserializer | float64 |\\n" + 
+    		"|---|\\n" + 
+    		"| org.apache.kafka.common.serialization.ByteArrayDeserializer | blob | \\n" + 
+    		"---\\n" + 
+    		"\\n" + 
     		"These deserializers are wrapped by extensions that catch exceptions of type "
     		+ "`org.apache.kafka.common.errors.SerializationException` to allow the operator to skip "
     		+ "over malformed messages. The used extensions do not modify the actual deserialization "
     		+ "function of the given base deserializers from the above table.\\n" +
     		"\\n" +
-    		"Users can override this behaviour and specify which deserializer to use by setting the " //$NON-NLS-1$
-    		+ "`key.deserializer` and `value.deserializer` properties. \\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
+    		"Users can override this behaviour and specify which deserializer to use by setting the "
+    		+ "`key.deserializer` and `value.deserializer` properties. \\n" + 
+    		"\\n" + 
     		
     		"# Committing received Kafka messages\\n"
     		+ "\\n"
     		+ "**a) The operator is not part of a consistent region**\\n"
     		+ "\\n"
-    		+ "When not explicitly specified, the operator sets the consumer property `auto.commit.enable` to `false` "
-    		+ "to disable auto-committing messages by the Kafka client. After tuple submission, the consumer operator commits the offsets of "
+    		+ "The consumer operator commits the offsets of "
     		+ "those Kafka messages that have been submitted as tuples. The frequency in terms of "
     		+ "number of tuples can be specified with the **commitCount** parameter. This parameter has a default value of 500. "
     		+ "Offsets are committed asynchronously.\\n"
-    		+ "\\n"
-    		+ "It is also possible to enable automatic committing of consumed Kafka messages. To achieve this, the consumer "
-    		+ "property `auto.commit.enable` must be set to true. In addition, the frequency in milliseconds can be adjusted "
-    		+ "via consumer property `auto.commit.interval.ms`, which has a default value of 5000.\\n"
     		+ "\\n"
     		+ "**b) The operator is part of a consistent region**\\n"
     		+ "\\n"
@@ -148,54 +154,57 @@ public class KafkaConsumerOperator extends AbstractKafkaConsumerOperator {
     		+ "processing of the operator finishes. Commit failures result in consistent region reset.\\n"
     		+ "\\n" +
     		
-    		"# Kafka's Group Management\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"The operator is capable of taking advantage of Kafka's group management functionality. " //$NON-NLS-1$
-    		+ "In order for the operator to use this functionality, the following requirements " //$NON-NLS-1$
+    		"# Kafka's Group Management\\n" + 
+    		"\\n" + 
+    		"The operator is capable of taking advantage of [https://kafka.apache.org/documentation/#intro_consumers|Kafka's group management function]. "
+    		+ "In order for the operator to use this functionality, the following requirements "
     		+ "must be met\\n"
     		+ "\\n"
-    		+ "* When the operator participates in a consistent region, the **consistentRegionAssignmentMode** "
-    		+ "parameter must have the value `GroupCoordinated`, and the **startPosition** parameter cannot have the "
-    		+ "value `Offset`. All other startPosition parameter values are supported."
-    		+ "In a consistent region, the consumer group must not have consumers outside of the consistent region.\\n"
-    		+ "* When the operator is not part of a consistent region, the **startPosition** parameter value must be "
-    		+ "`Default`, or not specified. The values `Beginning`, `End`, and `Time` are not supported.\\n"
-    		+ "* None of the topics specified by the **topics** parameter can specify which partition to be assigned to "
-    		+ "(the **partition** parameter cannot be used).\\n"
+    		+ "* The `group.id` consumer property must be set to define, which operators belong to a consumer group. "
+    		+ "This can be done via property file, app option, or the **groupId** parameter.\\n"
+    		+ "* The operator must not be configured with the optional input port.\\n"
+    		+ "* The parameter **partition** must not be used.\\n"
+    		+ "* **Restriction, when not in a consistent region:** The **startPosition** parameter must have the "
+    		+ "value `Default` or must not be specified ([https://github.com/IBMStreams/streamsx.kafka/issues/94|issue 94]).\\n"
     		+ "\\n"
-    		+ "In addition to the above, the application needs to set the `group.id` Kafka consumer property or "
-    		+ "the **groupId** parameter in order to assign the KafkaConsumer to a specific group. \\n"
+    		+ "The other way round, group management is inactive in following cases\\n"
+    		+ "* when no group ID is specified, or\\n"
+    		+ "* when the operator is configured with input port, or\\n"
+    		+ "* when the **partition** parameter is specified, or\\n"
+    		+ "* **only when not in consistent region**, the value of the **startPosition** is different from `Default`.\\n"
     		+ "\\n"
-    		+ "# Consistent Region Support\\n" +  //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-    		"The `KafkaConsumer` operator can participate in a consistent region. The operator " //$NON-NLS-1$
-    		+ "can be the start of a consistent region. Both operator driven and periodic checkpointing " //$NON-NLS-1$
-    		+ "are supported. If using operator driven, the **triggerCount** parameter must be set to " //$NON-NLS-1$
+    		+ "In a consistent region, a consumer group must not have consumers outside of the consistent region, "
+    		+ "for example in a different Streams job. The state of group management is indicated by the custom "
+    		+ "metric **isGroupManagementActive**, where `1` indicates that group management is active.\\n"
+    		+ "\\n"
+    		+ "# Consistent Region Support\\n" + 
+    		"\\n" + 
+    		"The `KafkaConsumer` operator can participate in a consistent region. The operator "
+    		+ "can be the start of a consistent region. Both operator driven and periodic triggering of the region "
+    		+ "are supported. If using operator driven, the **triggerCount** parameter must be set to "
     		+ "indicate how often the operator should initiate a consistent region.\\n"
     		+ "\\n"
-    		+ "In a consistent region, the topic partitions to be consumed can be assigned statically by the operator itself or by Kafka. "
-    		+ "This can be controlled via the operator parameter **consistentRegionAssignmentMode**. When partitions are assigned by Kafka, "
-    		+ "the operator is part of a Kafka consumer group together with other consumer operators that share a group ID. "
-    		+ "All consumer operators of a consumer group must belong to the same consistent region. A consistent region can have "
-    		+ "multiple consumer groups. When the consumers of a consumer group rebalance the partition assignment, for example, when a Streams job is "
-    		+ "submitted, multiple resets of the consistent region can occur when the consumers start up. It is recommended to set the "
+    		+ "When a group-ID is specified via the consumer property `group.id` or the **groupId** parameter, the operator "
+    		+ "participates automatically in a consumer group defined by the group ID. A consistent region can have "
+    		+ "multiple consumer groups.\\n"
+    		+ "\\n"
+    		+ "When the consumers of a consumer group rebalance the partition assignment, for example, immediately after job "
+    		+ "submission, or when the broker node being the group's coordinator is shutdown, multiple resets of the consistent "
+    		+ "region can occur when the consumers start up. It is recommended to set the "
     		+ "`maxConsecutiveResetAttempts` parameter of the `@consistent` annotation to a higher value than the default value of 5.\\n"
     		+ "\\n"
-    		+ "Details about consumer groups with consistent region can be found here: [https://ibmstreams.github.io/streamsx.kafka/docs/user/ConsumerGroupConsistentRegion/]\\n"
+    		+ "On drain, the operator will commit offsets. The drain duration of the last drain and the maximum drain "
+    		+ "duration in milliseconds is kept in the custom metrics **drainTimeMillis** and **drainTimeMillisMax**. "
+    		+ "These metrics are only present when the operator participates in a consistent region.\\n"
     		+ "\\n"
-    		+ "When Kafka's group management is active, the consistent region is reset when rebalancing occurs during consumption of messages.\\n"
-    		+ "\\n"
-    		+ "On drain, the operator will flush messages and commit offsets. The drain duration of the last drain and the maximum drain "
-    		+ "duration in milliseconds is kept in the custom metrics **drainTimeMillis** and **drainTimeMillisMax**.\\n"
-    		+ "\\n"
-    		+ "On checkpoint, the operator will save the last offset for each topic-partition that it is assigned to. In the " //$NON-NLS-1$
-    		+ "event of a reset, the operator will seek to the saved offset for each topic-partition and " //$NON-NLS-1$
-    		+ "begin consuming messages from that point." + //$NON-NLS-1$
-    		"\\n" +  //$NON-NLS-1$
-			"\\n" +  //$NON-NLS-1$
+    		+ "On checkpoint, the operator will save the last offset for each topic-partition that it is assigned to. In the "
+    		+ "event of a reset, the operator will seek to the saved offset for each topic-partition and "
+    		+ "begin consuming messages from that point." +
+    		"\\n" + 
+			"\\n" + 
     		
-			"# Error Handling\\n" +  //$NON-NLS-1$
-			"\\n" +  //$NON-NLS-1$
-			"Many exceptions thrown by the underlying Kafka API are considered fatal. In the event that Kafka throws " //$NON-NLS-1$
-			+ "an exception, the operator will restart.\\n"; //$NON-NLS-1$
+			"# Error Handling\\n" + 
+			"\\n" + 
+			"Many exceptions thrown by the underlying Kafka API are considered fatal. In the event that Kafka throws "
+			+ "an exception, the operator will restart.\\n";
 }

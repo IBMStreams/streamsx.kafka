d7604d9a87fafb3fad75b22fbf31c18926154e78
RolefH <rolef.heinrich@de.ibm.com>
2019-03-21 16:30:50 +0100

    v2 features enabled

--- com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/Features.java
+++ com.ibm.streamsx.kafka/impl/java/src/com/ibm/streamsx/kafka/Features.java
@@ -12,11 +12,12 @@ public class Features {
      * When set to false, group management is automatically disabled when startPosition != Default and not in a CR. 
      * This feature requires a JobControlPlane.
      */
-    public static boolean ENABLE_NOCR_CONSUMER_GRP_WITH_STARTPOSITION = false;
+    public static boolean ENABLE_NOCR_CONSUMER_GRP_WITH_STARTPOSITION = !SystemProperties.isLegacyBehavior();
+
     /**
      * When set to true, the consumer does not seek to initial startPosition when not in consistent region.
      * When false, the consumer seeks to what startPosition is after every restart.
      * This feature requires a JobControlPlane.
      */
-    public static boolean ENABLE_NOCR_NO_CONSUMER_SEEK_AFTER_RESTART = false;
+    public static boolean ENABLE_NOCR_NO_CONSUMER_SEEK_AFTER_RESTART = !SystemProperties.isLegacyBehavior();
 }
